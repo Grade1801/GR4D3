36dc0565c567ff67cb195424ce380d83b41635b0
Grade1801 <Grade@gmx.net>
2017-04-26 14:32:29 +0200

    Delete ready.js
    this file is moved to ./events/ready.js

--- commands/main/ready.js
+++
@@ -1,6 +0,0 @@
-const config = require (`./../../config/config.json`);
-
-// the ready function to share in the cli
-module.exports = () => {
-		console.log(`GR4D3 is now ready on verson ${config.version} and listening for your commands :)`);
-};
