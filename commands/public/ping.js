4782cb1589568725d1e7f037cc605f607ee3e967
Grade1801 <Grade@gmx.net>
2017-04-27 16:10:23 +0200

    testing commit and push to Grade branch

--- commands/public/ping.js
+++ commands/public/ping.js
@@ -2,5 +2,7 @@ module.exports = (message, prefix) => {
   if (message.content.startsWith (prefix + `ping`)) {
   message.channel.sendMessage(`pong to ${message.author} at ${message.channel}`);
   };
-  
+
 };
+
+// Test commit and push to Grade branch
